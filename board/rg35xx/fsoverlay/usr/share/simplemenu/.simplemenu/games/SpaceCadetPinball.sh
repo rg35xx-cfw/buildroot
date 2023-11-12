4526c825dd3e13f29d42bb62e8a5c2b241c395d4
acmeplus <ctalon@gmail.com>
2023-11-12 04:20:58 +0000

    Removing duplicated ports. Added drastic stylus support

--- board/rg35xx/fsoverlay/usr/share/simplemenu/.simplemenu/games/SpaceCadetPinball.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-export SDL_GAMECONTROLLERCONFIG=$(grep "RG35XX" "${HOME}/.config/gamecontrollerdb.txt")
-
-SDL_FULLSCREEN=1 SpaceCadetPinball
-
--- board/rg35xx/fsoverlay/usr/share/simplemenu/.simplemenu/games/VVVVVV.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-
-export SDL_GAMECONTROLLERCONFIG=$(grep "RG35XX" "${HOME}/.config/gamecontrollerdb.txt")
-
-VVVVVV 
-
